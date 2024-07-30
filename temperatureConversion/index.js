7dfb2a97c65bd42a6e69614198157029ab1d168d
nepalhomeland <ashokabrother@gmail.com>
2024-07-30 18:57:33 +0545

    mobile responsive version of Temperature Converter

--- temperatureConversion/index.js
+++ temperatureConversion/index.js
@@ -2,17 +2,15 @@ function convertTemperature() {
     const temperatureInput = document.getElementById('temperature').value;
     const unit = document.getElementById('unit').value;
     let result;
-
+  
     if (unit === 'C') {
-        // Convert Celsius to Fahrenheit
-        result = (temperatureInput * 9/5) + 32;
-        document.getElementById('result').innerText = `${temperatureInput} °C = ${result.toFixed(2)} °F`;
+      // Convert Celsius to Fahrenheit
+      result = (temperatureInput * 9/5) + 32;
+      document.getElementById('result').innerText = `${temperatureInput} °C = ${result.toFixed(2)} °F`;
     } else {
-        // Convert Fahrenheit to Celsius
-        result = (temperatureInput - 32) * 5/9;
-        document.getElementById('result').innerText = `${temperatureInput} °F = ${result.toFixed(2)} °C`;
+      // Convert Fahrenheit to Celsius
+      result = (temperatureInput - 32) * 5/9;
+      document.getElementById('result').innerText = `${temperatureInput} °F = ${result.toFixed(2)} °C`;
     }
-}
-
-
-//this branch to be deployed into vercel
+  }
+  
